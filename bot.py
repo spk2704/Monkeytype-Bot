5e9df9a7cc2760ce7ffb71631faee54487765380
JesseStan <stany2704@gmail.com>
2026-02-05 19:47:47 +0400

    ll

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -11,12 +8,10 @@ driver.get("https://monkeytype.com")
 time.sleep(5)
 
 textarea = driver.find_element(By.XPATH, "//textarea[@id='wordsInput']")
+raw_words = driver.find_element(By.ID, "words")
+words = raw_words.text.split()
 
-raw_words_first = driver.find_element(By.ID, "words")
-
-words_first = raw_words_first.text.split()
-
-for word in words_first:
+for word in words:
     textarea.send_keys(word + " ")
     time.sleep(0.3)
 
